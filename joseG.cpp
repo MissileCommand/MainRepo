44f557310fb5a366c5275828c4afd854e4a4f8e6
jgarcia4 <jhazard18@yahoo.com>
2016-05-30 18:40:01 -0700

    added a bunch of texture stuff

--- joseG.cpp
+++ joseG.cpp
@@ -14,7 +14,8 @@ extern "C" {
 //extern void init_opengl();
 
 GLuint cityTexture;
-
+GLuint streetTexture;
+GLuint civilianTexture;
 
 void initStruc(Game *game)
 {
@@ -40,6 +41,7 @@ void destroyCity(Game *game, int citynum)
 {
 	Shape *dcity = &game->structures.city[citynum];
 	dcity->alive = 0;
+	makeCivilian(game,dcity->width,dcity->height);
 	return;
 }
 
@@ -58,13 +60,15 @@ void renderStruc(Game *game)
 	glTranslatef(s->center.x, s->center.y, 0);
 	w = s->width;
 	h = s->height;
+	glBindTexture(GL_TEXTURE_2D, streetTexture);
 	glBegin(GL_QUADS);
-	glVertex2i(-w,-h);
-	glVertex2i(-w, h);
-	glVertex2i( w, h);
-	glVertex2i( w,-h);
+	glTexCoord2f(0.0f, 1.0f); glVertex2i(-w,-h);
+	glTexCoord2f(0.0f, 0.0f); glVertex2i(-w, h);
+	glTexCoord2f(1.0f, 0.0f); glVertex2i( w, h);
+	glTexCoord2f(1.0f, 1.0f); glVertex2i( w,-h);
 	glEnd();
 	glPopMatrix();
+	glBindTexture(GL_TEXTURE_2D, 0);
 
 	//draw cities
 	Shape *c;
@@ -82,23 +86,85 @@ void renderStruc(Game *game)
 		glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
 		//glColor4ub(255,255,255,255);
 		glBegin(GL_QUADS);
-			glTexCoord2f(1.0f, 1.0f); glVertex2i(-w2,-h2);
-			glTexCoord2f(1.0f, 0.0f); glVertex2i(-w2, h2);
-			glTexCoord2f(0.0f, 0.0f); glVertex2i( w2, h2);
-			glTexCoord2f(0.0f, 1.0f); glVertex2i( w2,-h2);
+			glTexCoord2f(0.0f, 1.0f); glVertex2i(-w2,-h2);
+			glTexCoord2f(0.0f, 0.0f); glVertex2i(-w2, h2);
+			glTexCoord2f(1.0f, 0.0f); glVertex2i( w2, h2);
+			glTexCoord2f(1.0f, 1.0f); glVertex2i( w2,-h2);
 		glEnd();
 		glDisable(GL_BLEND);
 		glPopMatrix();
 		glBindTexture(GL_TEXTURE_2D, 0);
 		}
 	}
+	//draw civilians
+	glColor3f(1.0,1.0,1.0);
+	glPushMatrix();
+	for (int i=0; i<game->nparticles; i++) {
+		Vec *g = &game->particle[i].part.center;
+		w = 20;
+		h = 20;
+		glBindTexture(GL_TEXTURE_2D, civilianTexture);
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+		glBegin(GL_QUADS);
+			glTexCoord2f(0.0f, 1.0f); glVertex2i(g->x-w, g->y-h);
+			glTexCoord2f(0.0f, 0.0f); glVertex2i(g->x-w, g->y+h);
+			glTexCoord2f(1.0f, 0.0f); glVertex2i(g->x+w, g->y+h);
+			glTexCoord2f(1.0f, 1.0f); glVertex2i(g->x+w, g->y-h);
+		glEnd();
+		glDisable(GL_BLEND);
+		glPopMatrix();
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+}
+
+void makeCivilian(Game *game, int x, int y)
+{
+	if (game->nparticles >= MAX_PARTICLES)
+		return;
+	//std::cout << "makeParticle()" << x << " " << y << std::endl;
+	//position of civilian
+	Particle *p = &game->particle[game->nparticles];
+	p->part.center.x = x;
+	p->part.center.y = y;
+	p->velocity.y = -4.0;
+	p->velocity.x =  0.5;
+	game->nparticles++;
+}
+
+void civilianPhysics(Game *game)
+{
+	Particle *p;
+	Structures *sh = &game->structures;
+	Shape *c;
+
+	for (int i=0; i<game->nparticles; i++) {
+		p = &game->particle[i];
+		//civilian position
+		p->part.center.x += p->velocity.x;
+		p->part.center.y += p->velocity.y;
+
+		//gravity
+		p->velocity.y -= GRAVITY;
+
+	//check for collision with floor
+	c = &sh->floor;
+	if (p->pos.y <= c->center.y+c->height) {
+		p->velocity.y *= -0.1;
+	}
+
+	//check for off-screen
+	if (p->part.center.y < 0.0) {
+			//std::cout << "off screen" << std::endl;
+			game->particle[i] = game->particle[game->nparticles-1];
+			game->nparticles--;
+	}
+	}
 }
 
 void renderBackground(GLuint starsTexture)
 {
-	//glColor3f(1.0, 1.0, 1.0);
 	glBindTexture(GL_TEXTURE_2D, starsTexture);
-	//glEnd();
 	glBegin(GL_QUADS);
 	glTexCoord2f(0.0f, 1.0f); glVertex2i(0, 0);
 	glTexCoord2f(0.0f, 0.0f); glVertex2i(0, WINDOW_HEIGHT);
@@ -115,4 +181,76 @@ void renderScores(Game *game)
     r.left = 50.0;
     r.center = 0;
     ggprint16(&r, 16, 0x00005599, "Score: %i", game->score);
+}
+
+unsigned char *buildAlphaData(Ppmimage *img)
+{
+	//add 4th component to RGB stream...
+	int i;
+	int a,b,c;
+	unsigned char *newdata, *ptr;
+	unsigned char *data = (unsigned char *)img->data;
+	newdata = (unsigned char *)malloc(img->width * img->height * 4);
+	ptr = newdata;
+	for (i=0; i<img->width * img->height * 3; i+=3) {
+		a = *(data+0);
+		b = *(data+1);
+		c = *(data+2);
+		*(ptr+0) = a;
+		*(ptr+1) = b;
+		*(ptr+2) = c;
+		//get largest color component...
+		//*(ptr+3) = (unsigned char)((
+		//              (int)*(ptr+0) +
+		//              (int)*(ptr+1) +
+		//              (int)*(ptr+2)) / 3);
+		//d = a;
+		//if (b >= a && b >= c) d = b;
+		//if (c >= a && c >= b) d = c;
+		//*(ptr+3) = d;
+		*(ptr+3) = (a|b|c);
+		ptr += 4;
+		data += 3;
+	}
+	return newdata;
+}
+
+GLuint makeTexture(GLuint texture, Ppmimage * image)
+{
+
+	glEnable(GL_TEXTURE_2D);
+	glGenTextures(1, &texture);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3,
+							image->width, image->height,
+							0, GL_RGB, GL_UNSIGNED_BYTE, image->data);
+	return texture;
+}
+
+GLuint makeTransparentTexture(GLuint texture, Ppmimage * image)
+{
+
+	glEnable(GL_TEXTURE_2D);
+	glGenTextures(1, &texture);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	glTexImage2D(GL_TEXTURE_2D, 0, 3,
+							image->width, image->height,
+							0, GL_RGB, GL_UNSIGNED_BYTE, image->data);
+	GLuint transparentTexture;
+	glGenTextures(1, &transparentTexture);
+
+	glBindTexture(GL_TEXTURE_2D, transparentTexture);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_NEAREST);
+	unsigned char *transparentData = buildAlphaData(image);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image->width, image->height, 0,
+							GL_RGBA, GL_UNSIGNED_BYTE, transparentData);
+	free(transparentData);
+	return transparentTexture;
 }
